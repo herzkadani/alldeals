74ce30760afc42af41b2596fe95de177846e1425
root <root@web-01.gk.wtf>
2022-02-08 11:58:18 +0100

    Fix path for api

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
 header('Access-Control-Allow-Origin: *');
-$name = shell_exec("python3 /opt/api.py");
+$name = shell_exec("python3 api.py");
 echo $name;
 ?>
