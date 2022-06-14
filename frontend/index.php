9cc55ef8271c42594db45f6287c111946996655f
Gian Klug <gian.klug@ict-scouts.ch>
2022-06-14 10:18:10 +0200

    Add GitHub banner

--- frontend/index.php
+++ frontend/index.php
@@ -69,6 +69,7 @@ foreach ($data as $key => $value) {
 </head>
 
 <body>
+<a href="https://github.com/herzkadani/alldeals" style="position: absolute; right: 0;"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>
     <header>
         <h1>AllDeals&nbsp;</h1><h3>Alle Tagesangebote kombiniert!</h3>
     </header>
