a7994393d699a422c43f0602a096fe16d3c87259
Gian Klug <gian.klug@ict-scouts.ch>
2024-05-07 08:55:52 +0200

    fix(frontend): simplify api

--- frontend/api.php
+++ frontend/api.php
@@ -1,25 +1,9 @@
 <?php
-
-#$json = file_get_contents("https://gianklug.com/alldeals/backend/index.php");
-#$json = shell_exec("../backend/api.py");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $deals_json = file_get_contents("/deals/deals-" . date('Y-m-d') . ".json");
-$data = json_decode($deals_json, true);
-
-$colors_json =  file_get_contents("brandcolors.json");
-$brandcolors = json_decode($colors_json, true);
-
-#if (!isset($_GET["action"])) {
-#  die("GET parameter 'action' missing.");
-#}
-#
-#$action = $_GET["action"];
-
-
-$htmloutput = '';
 
 header("Content-Type: application/json");
-echo (json_encode($data));
+echo $deals_json;
